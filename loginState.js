1dfe74764843eb76c6d897bc308abe22ffc6eb22
Akshat Gupta <akshatgtc@gmail.com>
2023-04-18 02:08:44 +0530

    final

--- loginState.js
+++ loginState.js
@@ -3,18 +3,22 @@ let signUpButton = document.getElementById("signupButton");
 let username = document.getElementById("username");
 let logoutElement = document.getElementById("logout");
 let currentUser = JSON.parse(localStorage.getItem("currentUser"));
+let rounded = document.getElementById("rounded");
+
 
 if (currentUser) {
   console.log(currentUser[0]);
   loginButton.setAttribute("style", "display: none");
   signUpButton.setAttribute("style", "display: none");
-  username.innerText = "Hii! "+currentUser[0].firstName;
+  username.innerText = currentUser[0].firstName.substring(0,1);
   logoutElement.innerText = "Logout";
+  rounded.setAttribute("style", "display: flex");
+
 }
 
  
 const logout = () => {
-    localStorage.removeItem("currentUser");
-    localStorage.setItem("isLoggedIn", false);
-    window.location.reload();
-}
+localStorage.removeItem("currentUser");
+localStorage.setItem("isLoggedIn", false);
+window.location.reload();
+};
